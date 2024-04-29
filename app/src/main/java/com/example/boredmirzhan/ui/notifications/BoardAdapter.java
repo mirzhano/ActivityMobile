54b5b6fb914ab6d976f9ede48cc8a5bd609330d2
theflexor <nurdanokenov@gmail.com>
2024-04-29 12:49:25 +0600

    first commit

---
+++ app/src/main/java/com/example/boredmirzhan/ui/notifications/BoardAdapter.java
@@ -0,0 +1,69 @@
+package com.example.boredmirzhan.ui.notifications;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.boredmirzhan.Prefs;
+import com.example.boredmirzhan.R;
+import com.example.boredmirzhan.databinding.ItemBoardBinding;
+
+public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> {
+    ItemBoardBinding binding;
+    NavController navController;
+
+    private int[] images =new int[]{
+            R.drawable.mood1,
+            R.drawable.pair,
+            R.drawable.man};
+
+    private String[] titles =new String[]{
+            "Стрессуешь на работе?",
+            "Хотите интересно провести время вместе?",
+            "Не знаешь чем себя занять?"};
+    @NonNull
+    @Override
+    public BoardAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        binding = ItemBoardBinding
+                .inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BoardAdapter.ViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView.getRootView());
+        }
+
+        public void bind(int position) {
+            binding.textBoard.setText(titles[position]);
+            binding.imageBoard.setImageResource(images[position]);
+            if(position==titles.length-1){
+                binding.btnStart.setVisibility(View.VISIBLE);
+            }else {
+                binding.btnStart.setVisibility(View.VISIBLE);
+            }
+            binding.btnStart.setOnClickListener(v->{
+                new Prefs((Activity) itemView.getContext()).saveBoardState();
+
+                navController = Navigation.findNavController((Activity) itemView.getContext(), R.id.nav_host_fragment_activity_main);
+                navController.navigate(R.id.action_navigation_notifications_to_navigation_home);
+            });
+        }
+    }
+}
